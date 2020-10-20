0d3b5db6fb2516332ec5dab351b40629a6e87f1f
Srishti0903 <srishtimishra09334@gmail.com>
2020-10-20 13:09:21 +0530

    To Do Application

---
+++ app/src/main/java/com/example/todoapp/AlertReceiver.java
@@ -0,0 +1,62 @@
+package com.example.todoapp;
+
+
+import android.annotation.TargetApi;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import static android.provider.Settings.System.DEFAULT_RINGTONE_URI;
+
+public class AlertReceiver extends BroadcastReceiver {
+    public static final String channelID = "channelID";
+    public static final String channelName = "Channel Name";
+    private NotificationManager mManager;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH);
+            if (mManager == null) {
+                mManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            }
+            mManager.createNotificationChannel(channel);
+
+            Intent i = new Intent(context, RingtonePlayingService.class);
+            i.setAction("ALARM_DISMISS");
+            PendingIntent p = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context.getApplicationContext(), channelID)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentTitle("TO DO")
+                    .setContentText("Alarm Working")
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setCategory(NotificationCompat.CATEGORY_ALARM);
+            builder.setColor(Color.BLUE);
+            builder.setAutoCancel(true);
+            builder.addAction(R.drawable.ic_launcher_foreground, "CANCEL", p);
+            mManager.notify(1, builder.build());
+        }
+
+
+        //NotificationHelper notificationHelper = new NotificationHelper(context);
+        //PendingIntent pendingIntent = PendingIntent.getBroadcast(context,0,intent,0);
+        //NotificationCompat.Builder nb = notificationHelper.getChannelNotification(pendingIntent);
+        //notificationHelper.getManager().notify(1, nb.build());
+    }
+}
